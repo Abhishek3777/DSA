2fb963320831d50eae0a06cf4fb51bb5780e0cab
Abhishek3777 <kr7abhishek04@gmail.com>
2025-10-20 13:05:16 +0530

    added greedy algorithms

---
+++ Greedy/Candy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        long long int totalCandies = 0;
+        vector<int> score(n, 1);
+        // left to right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1] && score[i] <= score[i - 1]) {
+                score[i] = score[i - 1] + 1;
+            }
+        }
+        /// right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1] && score[i] <= score[i + 1]) {
+                score[i] = score[i + 1] + 1;
+            }
+        }
+        for (int num : score) {
+            totalCandies += num;
+        }
+        return totalCandies;
+    }
+};
