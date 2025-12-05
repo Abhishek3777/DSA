b5f44236a9a461fe5db89f255baddfb0bf3f7742
Abhishek3777 <kr7abhishek04@gmail.com>
2025-12-05 21:17:35 +0530

    cf update

---
+++ Codeforces/Rating900/Problem15.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        vector<long long> a(n);
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        // logic
+        long long steps = 0;
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            while (a[i] >= a[i + 1])
+            {
+                steps++;
+                a[i] /= 2;
+                if (a[i] == 0)
+                    break;
+            }
+            if (a[i] == 0 && a[i + 1] == 0)
+            {
+                steps = -1;
+                break;
+            }   
+        }
+
+        cout << steps << endl;
+    }
+
+    return 0;
+}
