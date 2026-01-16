1daf334d666b95c60b489abc1bb3874123ca6c7f
Abhishek3777 <kr7abhishek04@gmail.com>
2026-01-17 00:07:35 +0530

    added linked list and comparators

--- Greedy/JumpGame.cpp
+++ Greedy/JumpGame.cpp
@@ -1,8 +1,12 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-
-
-    return 0;
-}
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxIndex = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > maxIndex)
+                return false;
+            maxIndex = max(maxIndex, nums[i] + i);
+        }
+        return true;
+    }
+};
